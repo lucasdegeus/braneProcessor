9d755198ee7d3fe12bb71bfe79af984c9ce7c1fb
lucasdegeus <lucasdegeus1999@gmail.com>
2021-05-26 00:08:40 +0200

    processor package

---
+++ readme.md
@@ -0,0 +1,44 @@
+# dataprocessor
+Dataprocessor is a brane package for manipulating data and using various statistical functions on this data.
+Think about packages such as Pandas and Numpy for Python. Currently only two functions are identified:
+    * A function to read a csv and return one column of this csv
+    * A function that computes the average for each key in a dictionary. The keys are represented as a list and the values as well.
+
+## Installation
+
+If on Linux or MacOS first run:
+
+``` chmod +x run.py ```
+
+Otherwise/then:
+
+```console
+brane build container.yml
+brane push dataprocessor 1.0.0
+```
+
+## Usage average
+Input parameters are:
+    * keys: an array of keys in a dictionary
+    * values: an array that should equal the length of keys and should contain the values associated with the keys.
+```brane
+import dataprocessor;
+
+let keys := ["Canada", "Canada", "United States of America"];
+let values := [100.0, 200.0, -50.0];
+
+average_over_dict_values(keys, values);
+average_over_dict_keys(keys, values);
+```
+
+## Usage csv
+Input parameters are:
+    * file: filename to read from
+    * separator: the seperator between columns. Usually a "," in a csv
+    * columname: the column to retrieve from this csv
+```brane
+read_csv_column(/data/<filename.csv>, ",", "columnname");
+```
+
+## Notes
+It is currently not possible to return tuples, dictionaries or multiple outputs in Brane. Therefore, the average function is split into two. ``average_over_dict_values`` returns the keys and ``average_over_dict_values`` the values corresponding with those keys.
---
+++ run.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import os
+import sys
+import yaml
+import pandas as pd
+from collections import defaultdict
+from typing import List
+
+def read_csv_column(filename: str, separator: str, columname: str) -> List[str]:
+    return list(pd.read_csv(filename,sep=separator).loc[:,columname].astype('string'))
+
+def average_over_dict(keys: List[str], values: List[float]) -> (List[str], List[float]):
+    assert(len(keys) == len(values))
+    # Group
+    key_value = defaultdict(list)
+    for i in range(len(values)):
+        key_value[keys[i]].append(values[i])
+    # Aggregate
+    result_keys = []
+    result_values = []
+    results = {}
+    for key in keys:
+        if (key not in results):
+            results[key] = True
+            result_keys.append(str(key))
+            result_values.append(float(sum(key_value[key]) / len(key_value[key])))
+    return (result_keys, result_values)
+
+if __name__ == "__main__":
+    command = sys.argv[1]
+    output = [float(1)]
+    if (command == "read_csv_column"):
+        argument_file = os.environ["FILENAME"]
+        argument_separator = os.environ["SEPARATOR"]
+        argument_column = os.environ["COLUMN"]
+        output = read_csv_column(argument_file, argument_separator, argument_column)
+        print("--> START CAPTURE")
+        print(yaml.dump({"output": output}))
+        print("--> END CAPTURE")
+
+    else:
+        argument_keys = [str(os.environ[f"KEYS_{i}"]) for i in range(int(os.environ["KEYS"]))]
+        argument_values = [float(os.environ[f"VALUES_{i}"]) for i in range(int(os.environ["VALUES"]))]
+        averages = average_over_dict(argument_keys, argument_values)
+        output = averages[0] if command == 'average_over_dict_keys' else averages[1]
+        print("--> START CAPTURE")
+        print(yaml.dump({"output": output}))
+        print("--> END CAPTURE")
